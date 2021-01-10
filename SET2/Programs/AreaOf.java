7ae343342013334502699a99486e5162c41b4318
Aromal S <aromalponmana@gmail.com>
2021-01-10 21:08:37 +0530

    Update AreaOf.java

--- SET2/Programs/AreaOf.java
+++ SET2/Programs/AreaOf.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class AreaOf {
 	
-	AreaOf(double length,double breadth){
+	void Area(double length,double breadth){
 		
 		String area=String.format("%.3f", (length*breadth));
 		System.out.println("Area of Rectange Is "+area+"sq.units");
 	}
 	
-	AreaOf(double radius){
+	void Area(double radius){
 		
 		double pi = 3.1415;
 		String area= String.format("%.3f", (pi*Math.pow(radius, 2)));
@@ -22,20 +22,17 @@ public class AreaOf {
 		
 		System.out.println("Enter radius of circle :");
 		radius =sc.nextDouble();
-		AreaOf circle = new AreaOf(radius);
+		AreaOf circle = new AreaOf();
+		circle.Area(radius);
+
 		
 		System.out.println("Enter  length and beadth of rectangle :");
 		length = sc.nextDouble();
 		breadth=sc.nextDouble();
-		AreaOf rectangle = new AreaOf(length,breadth);
+		AreaOf rectangle = new AreaOf();
+		rectangle.Area(length, breadth);
 		
-
-
-		
-		
-		
-		
-		
+	
 	}
 	
 
