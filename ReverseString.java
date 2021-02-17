9df3ce33bd8c1a58254336de75ff3f84a9ef19b1
Aromal S <aromalponmana@gmail.com>
2021-02-17 13:55:06 +0530

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,15 @@
+public class ReverseString {
+	public ReverseString(String str) {
+		char ch[]=str.toCharArray();
+		String rev ="";
+		
+		for (int i = ch.length-1; i >= 0; i--) {
+			rev+=ch[i];
+		}
+		System.out.println(rev);
+	}
+	public static void main(String[] args) {
+		String name="Aromal";
+		new ReverseString(name);
+	}
+}
